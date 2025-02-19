fb6b77623c52e0c38e50e08271514343c4a1e47a
Jonathan Davidson <jonathan.davidson@canterbury.ac.nz>
2025-02-20 12:58:16 +1300

    make tests a bit faster

--- tests/linz_s3_filter_tests.rs
+++ tests/linz_s3_filter_tests.rs
@@ -1,8 +1,18 @@
 use linz_s3::linz_s3_filter::{Dataset, LinzBucket};
+use std::sync::Once;
 
+static INIT: Once = Once::new();
+
+fn init_logger() {
+    INIT.call_once(|| {
+        env_logger::builder().is_test(true).init();
+    });
+}
 #[tokio::test]
 async fn test_get_tiles_from_lat_lon_empty() {
-    let dataset = Dataset::Elevation;
+    init_logger();
+
+    let dataset = Dataset::Imagery;
     let linz_bucket = LinzBucket::initialise_catalog(dataset).await;
     let lat = 40.9006;
     let lon = 174.8860;
@@ -11,17 +21,21 @@ async fn test_get_tiles_from_lat_lon_empty() {
 }
 #[tokio::test]
 async fn test_get_tiles_from_lat_lon() {
-    let dataset = Dataset::Imagery;
+    init_logger();
+
+    let dataset = Dataset::Elevation;
     let linz_bucket = LinzBucket::initialise_catalog(dataset).await;
-    let lat = -40.9006;
-    let lon = 174.8860;
+    let lat = -45.0;
+    let lon = 167.0;
     let tiles = linz_bucket.get_tiles_from_lat_lon(lat, lon).await;
     assert!(!tiles.is_empty());
 }
 
 #[tokio::test]
 async fn test_get_tiles_from_lat_lon_range() {
-    let dataset = Dataset::Imagery;
+    init_logger();
+
+    let dataset = Dataset::Elevation;
     let linz_bucket = LinzBucket::initialise_catalog(dataset).await;
     let lat1 = -40.9006;
     let lon1 = 174.8860;
